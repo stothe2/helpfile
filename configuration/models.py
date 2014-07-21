93f0750054e54081f800f0d08b225b1b07fd40a5
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-21 10:57:22 -0700

    Update models.py
    wip

--- configuration/models.py
+++ configuration/models.py
@@ -1,30 +1,24 @@
 from django.db import models
 
 # Create your models here.
-class APMControls(models.Model):
+class Controls(models.Model):
 	item_name = models.CharField(max_length=200)
 	feature = models.TextField(max_length=200)
 	settings = models.TextField()
 	comments = models.TextField(blank=True)
-	#extra = models.TextField(blank=True)
-
-class PMControls(models.Model):
-	item_name = models.CharField(max_length=200)
-	feature = models.TextField(max_length=200)
-	settings = models.TextField()
-	comments = models.TextField(blank=True)
-
-class TMControls(models.Model):
-	item_name = models.CharField(max_length=200)
-	feature = models.TextField(max_length=200)
-	settings = models.TextField()
-	comments = models.TextField(blank=True)
-
-class UIControls(models.Model):
-	item_name = models.CharField(max_length=200)
-	feature = models.TextField(max_length=200)
-	settings = models.TextField()
-	comments = models.TextField(blank=True)
+	#additional_comments = models.TextField(blank=True)
 
 	def __unicode__(self):
 		return self.item_name
+
+	def get_model_name(self):
+		return self.__class__.__name__
+
+	class Meta:
+		abstract=True
+
+class APM(Controls):
+	pass
+
+class PM(Controls):
+	pass
