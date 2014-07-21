8a0fe6158176eae60b42fb4cda2dc87d6dd159a3
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-21 10:55:57 -0700

    Update views.py
    wip

--- configuration/views.py
+++ configuration/views.py
@@ -3,19 +3,15 @@ from django.template import RequestContext, loader
 from django.http import HttpResponse
 from django.views import generic
 
-from configuration.models import APMControls, PMControls, TMControls, UIControls
+from configuration.models import APM
 
 # Create your views here.
-def index(request):
-	apmcontrols_list = APMControls.objects.all().order_by('-id')
-	template = loader.get_template('configuration/index.html')
-	context = RequestContext(request, {
-		'apmcontrols_list': apmcontrols_list,
-		})
-	return HttpResponse(template.render(context))
+class IndexView(generic.ListView):
+	model = APM
+	template_name = 'configuration/index.html'
 
-def detail(request, apmcontrols_id):
-	item = APMControls.objects.get(id=apmcontrols_id)
+def detail(request, apm_id):
+	item = APM.objects.get(id=apm_id)
 	itemSet = item.settings.split(";")
 	itemCom = item.comments.split(";")
 	firstSet = itemSet[0]
@@ -27,7 +23,7 @@ def detail(request, apmcontrols_id):
 	countStr = str(count)
 	template = loader.get_template('configuration/detail.html')
 	context = RequestContext(request, {
-		'apmcontrols_id': apmcontrols_id,
+		'apm_id': apm_id,
 		'item': item,
 		'itemSet': itemSet,
 		'countStr': countStr,
@@ -37,3 +33,42 @@ def detail(request, apmcontrols_id):
 		'combined': combined,
 		})
 	return HttpResponse(template.render(context))
+
+'''
+class APMDetailView(generic.DetailView):
+	model = APM
+	template_name = 'configuration/detail.html'
+
+	def get_queryset(self):
+		queryset = super(APMDetailView, self).get_queryset().filter(pk=self.kwargs['pk'])
+		return queryset
+
+	def get_context_data(self, **kwargs):
+		itemSet = self.get_queryset().settings.split(';')
+		itemCom = comments.split(';')
+		firstSet = itemSet[0]
+		otherSet = itemSet[1:]
+		firstCom = itemCom[0]
+		otherCom = itemCom[1:]
+		combined = zip(otherSet, otherCom)
+		count = len(itemSet)
+		countStr = str(count)
+
+		extra_context = {
+			'firstSet': firstSet,
+			'firstCom': firstCom, 
+			'countStr': countStr,
+			'other': other,
+		}
+
+		context = super(APMDetailView, self).get_context_data(**kwargs)
+		
+		for key in extra_context:
+			context[key] = extra_context[key]
+
+		return context
+'''
+
+def HomePageView(request):
+	names = object.__class__.name__
+	return HttpResponse(context)
