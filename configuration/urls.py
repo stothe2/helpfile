35dd7cba1559da17295bfe04da7f3be5f3ba48ce
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-21 10:56:44 -0700

    Update urls.py
    wip

--- configuration/urls.py
+++ configuration/urls.py
@@ -4,8 +4,9 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from configuration import views
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-	url(r'^(?P<apmcontrols_id>\d+)/$', views.detail, name='detail')
+	url(r'^$', views.IndexView.as_view(), name='index'),
+	#url(r'^controls/apm/$', views.IndexView.as_view(), name='index')
+	url(r'^(?P<apm_id>\d+)/$', views.detail, name='detail')
 )
 
 urlpatterns += staticfiles_urlpatterns()
