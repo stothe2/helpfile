56c919aeee12bd4cc24d316fa6926872662ae849
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-07-24 15:26:23 -0700

    Update admin.py

--- configuration/admin.py
+++ configuration/admin.py
@@ -1,18 +1,66 @@
 from django.contrib import admin
-from configuration.models import APMControls
+from configuration.models import APM, PM, TM, UI
 
-class APMControlsAdmin(admin.ModelAdmin):
+class APMAdmin(admin.ModelAdmin):
 	# display order for admin site
 	fieldsets = [
 		(None, {'fields': ['item_name']}),
 		(None, {'fields': ['feature']}),
 		(None, {'fields': ['settings']}),
 		(None, {'fields': ['comments']}),
-		#(None, {'fields': ['extra']}),
+		(None, {'fields': ['additional_comments']})
 	]
 	# display all fields
-	list_display = ('item_name', 'feature', 'settings', 'comments')
+	list_display = ('item_name', 'feature', 'settings', 'comments', 'additional_comments')
 	# add search capability
 	search_fields = ['item_name']
 
-admin.site.register(APMControls, APMControlsAdmin)
+admin.site.register(APM, APMAdmin)
+
+class PMAdmin(admin.ModelAdmin):
+	# display order for admin site
+	fieldsets = [
+		(None, {'fields': ['item_name']}),
+		(None, {'fields': ['feature']}),
+		(None, {'fields': ['settings']}),
+		(None, {'fields': ['comments']}),
+		(None, {'fields': ['additional_comments']}),
+	]
+	# display all fields
+	list_display = ('item_name', 'feature', 'settings', 'comments', 'additional_comments')
+	# add search capability
+	search_fields = ['item_name']
+
+admin.site.register(PM, PMAdmin)
+
+class TMAdmin(admin.ModelAdmin):
+	# display order for admin site
+	fieldsets = [
+		(None, {'fields': ['item_name']}),
+		(None, {'fields': ['feature']}),
+		(None, {'fields': ['settings']}),
+		(None, {'fields': ['comments']}),
+		(None, {'fields': ['additional_comments']}),
+	]
+	# display all fields
+	list_display = ('item_name', 'feature', 'settings', 'comments', 'additional_comments')
+	# add search capability
+	search_fields = ['item_name']
+
+admin.site.register(TM, TMAdmin)
+
+class UIAdmin(admin.ModelAdmin):
+	# display order for admin site
+	fieldsets = [
+		(None, {'fields': ['item_name']}),
+		(None, {'fields': ['feature']}),
+		(None, {'fields': ['settings']}),
+		(None, {'fields': ['comments']}),
+		(None, {'fields': ['additional_comments']}),
+	]
+	# display all fields
+	list_display = ('item_name', 'feature', 'settings', 'comments', 'additional_comments')
+	# add search capability
+	search_fields = ['item_name']
+
+admin.site.register(UI, UIAdmin)
