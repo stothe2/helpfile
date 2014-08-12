9b0a207ac9e1930fece128e3fd84c4a1d3bc1e9e
Sachi Sanghavi <sanghavi.sachi@gmail.com>
2014-08-12 11:01:56 -0700

    Integrate summernote editor

--- configuration/admin.py
+++ configuration/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from configuration.models import APM, PM, TM, UI
 
-class APMAdmin(admin.ModelAdmin):
+class APMAdmin(SummernoteModelAdmin):
 	# display order for admin site
 	fieldsets = [
+		#(None, {'fields': ['fake_id']}),
 		(None, {'fields': ['item_name']}),
 		(None, {'fields': ['feature']}),
 		(None, {'fields': ['settings']}),
 		(None, {'fields': ['comments']}),
-		(None, {'fields': ['additional_comments']})
+		(None, {'fields': ['additional_comments']}),
 	]
 	# display all fields
 	list_display = ('item_name', 'feature', 'settings', 'comments', 'additional_comments')
@@ -17,9 +19,10 @@ class APMAdmin(admin.ModelAdmin):
 
 admin.site.register(APM, APMAdmin)
 
-class PMAdmin(admin.ModelAdmin):
+class PMAdmin(SummernoteModelAdmin):
 	# display order for admin site
 	fieldsets = [
+		#(None, {'fields': ['fake_id']}),
 		(None, {'fields': ['item_name']}),
 		(None, {'fields': ['feature']}),
 		(None, {'fields': ['settings']}),
@@ -33,9 +36,10 @@ class PMAdmin(admin.ModelAdmin):
 
 admin.site.register(PM, PMAdmin)
 
-class TMAdmin(admin.ModelAdmin):
+class TMAdmin(SummernoteModelAdmin):
 	# display order for admin site
 	fieldsets = [
+		#(None, {'fields': ['fake_id']}),
 		(None, {'fields': ['item_name']}),
 		(None, {'fields': ['feature']}),
 		(None, {'fields': ['settings']}),
@@ -49,9 +53,10 @@ class TMAdmin(admin.ModelAdmin):
 
 admin.site.register(TM, TMAdmin)
 
-class UIAdmin(admin.ModelAdmin):
+class UIAdmin(SummernoteModelAdmin):
 	# display order for admin site
 	fieldsets = [
+		#(None, {'fields': ['fake_id']}),
 		(None, {'fields': ['item_name']}),
 		(None, {'fields': ['feature']}),
 		(None, {'fields': ['settings']}),
